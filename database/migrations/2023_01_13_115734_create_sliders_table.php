161c2c63a258561fdd8b4ecf3f08c5bd9a656bac
ThonyDang <thony7321@hotmail.fr>
2023-01-13 18:38:11 +0100

    ajout migration page et sliders

---
+++ database/migrations/2023_01_13_115734_create_sliders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->string('subtitle',255);
+            $table->string('media',255);
+            $table->string('uri',255);
+            $table->dateTime('start_ad');
+            $table->dateTime('start_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
