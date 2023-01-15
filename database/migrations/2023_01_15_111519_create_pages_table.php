3b70fe78c5a3058dc13ace498f389b6a22c931fa
matrox471 <matthieuf96@hotmail.com>
2023-01-15 12:51:59 +0100

    ajout migration pages

---
+++ database/migrations/2023_01_15_111519_create_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('author');
+            $table->string('description');
+            $table->string('keywords');
+            $table->boolean('enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
