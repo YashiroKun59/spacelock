7009d1bb2f20f04548c870c4ea6d9991f5f5e0f8
matrox471 <matthieuf96@hotmail.com>
2023-01-15 21:56:43 +0100

    cleaned the code a bit + switched to bash

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
-#!/usr/bin/env sh
-
+#!/usr/bin/env bash
 # setting up composer
-echo "1/4 Installing composer addons..."
-ci="$(composer install &> /dev/null)"
+echo "1/4 Installing composer's addons..."
+composer install &> /dev/null
 echo "Done."
-
 # copying the .env.exemple file to .env
 echo "2/4 Copying env file..."
-#cp .env.example .env
+cp .env.example .env
 echo "Done."
-
 # doing the base migration
 echo "3/4 running migrations :"
 pam=$(php artisan migrate)
@@ -26,18 +23,15 @@ if [ "$pam" = "$pamerrorstr" ]; then
         exit 1
     fi
 fi
-
 # setting up backpack
 # and
 # creating backpack admin user
-
-echo "installing backpack..."
+# TODO implement a check if backpack installed succeed or not
+echo "4/4 installing backpack..."
 { echo "yes"; echo "admin"; echo "admin@email.com"; echo "123";  echo "no" ; echo "no" ; } | php artisan backpack:install > /dev/null
 echo "Done."
-echo ""
-echo ""
+echo "--INSTALLATION FINISHED--"
 echo "backpack user : admin"
 echo "backpack email : admin@email.com"
 echo "backpack password: 123"
-
 # echo "Done."
