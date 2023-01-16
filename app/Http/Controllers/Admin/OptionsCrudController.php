b8ddcf13b535fb2b71c079d798b8e39d4eb248d4
matrox471 <matthieuf96@hotmail.com>
2023-01-16 13:36:44 +0100

    typo

--- app/Http/Controllers/Admin/OptionsCrudController.php
+++ app/Http/Controllers/Admin/OptionsCrudController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\OptionsRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
+
 /**
  * Class OptionsCrudController
  * @package App\Http\Controllers\Admin
