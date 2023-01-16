25bf8adc2f85941bdfdc1d206d9989fce1c7de01
matrox471 <matthieuf96@hotmail.com>
2023-01-16 16:01:18 +0100

    added app key generation function

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # setting up composer
-echo "1/4 Installing composer's addons..."
+echo "1/5 Installing composer's addons..."
 composer install &> /dev/null
 echo "Done."
 # copying the .env.exemple file to .env
-echo "2/4 Copying env file..."
+echo "2/5 Copying env file..."
 cp .env.example .env
 echo "Done."
+# generating the app key
+echo "3/5 Generating app key"
+php artisan key:generate --ansi &> /dev/null
+echo "Done."
 # doing the base migration
-echo "3/4 running migrations :"
+echo "4/5 running migrations :"
 pam=$({ echo "yes";}|php artisan migrate)
 pamerrorstr="   INFO  Nothing to migrate.  "
 pam=$(echo "$pam" | grep "Nothing")
@@ -27,7 +31,7 @@ fi
 # and
 # creating backpack admin user
 # TODO implement a check if backpack installed succeed or not
-echo "4/4 installing backpack..."
+echo "5/5 installing backpack..."
 { echo "yes"; echo "admin"; echo "admin@email.com"; echo "123";  echo "no" ; echo "no" ; } | php artisan backpack:install > /dev/null
 echo "Done."
 echo "--INSTALLATION FINISHED--"
