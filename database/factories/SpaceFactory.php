74fbdae309afdddb82f54b3b866e9c4e5ca49e3e
matrox471 <matthieuf96@hotmail.com>
2023-01-16 22:02:53 +0100

    added space factory

---
+++ database/factories/SpaceFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Space>
+ */
+class SpaceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $site=DB::table('sites')->count('id');
+        $price=DB::table('prices')->count('id');
+        $spacetype=DB::table('spacetypes')->count('id');
+        return [
+
+            'nickname'=>fake()->name(),
+            'description'=>fake()->text(120),
+            'picture'=>fake()->imageUrl(),
+            'length'=>fake()->numberBetween(100,800),
+            'width'=>fake()->numberBetween(100,800),
+            'height'=>fake()->numberBetween(100,800),
+            'enabled'=>fake()->boolean(90),
+            'site_id'=>fake()->numberBetween(1,$site),
+            'spacetype_id'=>fake()->numberBetween(1,$spacetype),
+            'price_id'=>fake()->numberBetween(1,$price),
+        ];
+    }
+}
