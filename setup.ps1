cc3993a4123dc70fee703aab627bc56ebb341a41
matrox471 <matthieuf96@hotmail.com>
2023-01-16 19:16:57 +0100

    added database status check + seed on fresh

--- setup.ps1
+++ setup.ps1
@@ -1,40 +1,63 @@
 #!/usr/bin/env pwsh
-Write-Output "1/6 Setting up environment..."
+
+
+# doing all the prep work in case this is not a new install
+Write-Output "1/7 Setting up environment..."
 Remove-Item -Recurse .\vendor\ >$null 2>&1
+Write-Output "Done."
+
+
 # copying the .env.exemple file to .env
-Write-Output "2/6 Copying env file..."
+Write-Output "2/7 Copying env file..."
 Copy-Item .env.example .env
 Write-Output "Done."
+
 # setting up composer
-Write-Output "3/6 Installing composer's addons..."
+Write-Output "3/7 Installing composer's addons..."
 composer install >$null 2>&1
 Write-Output "Done."
+
+
 # generating the app key
-Write-Output "4/6 Generating app key"
+Write-Output "4/7 Generating app key"
 php artisan key:generate --ansi >$null 2>&1
 Write-Output "Done."
+
+
+# checking database status
+Write-Output "5/7 Checking database status"
+$dbstatus=$((php artisan db:show | findstr "SQLSTATE\[HY000\] \[2002\]").count)
+if ($dbstatus -eq 1){
+    Write-Output "Please start the database or check your .env file."
+    EXIT 1
+}
+Write-Output "Database: online."
+Write-Output "Done."
+
+
 # doing the base migration
-Write-Output "5/6 running migrations :"
+Write-Output "6/7 running migrations :"
 $pam=$("yes`r`n"|php artisan migrate)
 $pamerrorstr="   INFO  Nothing to migrate.  "
 $pam=$(Write-Output $pam[1] | findstr "Nothing to migrate")
 if ($pam -eq $pamerrorstr) {
-    $userInput= Read-Host -Prompt "Nothing happened, do you want to run a migrate:fresh ? (y/N) "
+    $userInput= Read-Host -Prompt "Nothing happened, do you want to run a migrate:fresh --seed ? (y/N) "
     if ($userInput -eq "y"){
         Write-Output "Running migrate:fresh..."
-        php artisan migrate:fresh
+        php artisan migrate:fresh --seed
         Write-Output "Done."
     }
     else {
         Write-Output "Aborting"
-        return 1
-    }
-}
+        EXIT 1
+    }}
+
+
 # setting up backpack
 # and
 # creating backpack admin user
 # TODO implement a check if backpack installed succeed or not
-Write-Output "6/6 installing backpack..."
+Write-Output "7/7 installing backpack..."
 php artisan backpack:install
 Write-Output "Done."
 Write-Output "--INSTALLATION DONE--"
--- setup.sh
+++ setup.sh
@@ -1,42 +1,67 @@
 #!/usr/bin/env bash
-# setting up composer
-echo "1/6 Setting up the environment..."
+
+
+# doing all the prep work in case this is not a new install
+echo "1/7 Setting up the environment..."
 rm ./vendor -r &> /dev/null
 echo "Done."
+
+
 # copying the .env.exemple file to .env
-echo "2/6 Copying env file..."
+echo "2/7 Copying env file..."
 cp .env.example .env
 echo "Done."
-echo "3/6 Installing composer's addons..."
+
+# Installing composer's addon
+echo "3/7 Installing composer's addons..."
 composer install &> /dev/null
 echo "Done."
+
+
 # generating the app key
-echo "4/6 Generating app key"
+echo "4/7 Generating app key"
 php artisan key:generate --ansi &> /dev/null
 echo "Done."
+
+
+# checking database status
+echo "5/7 Checking database status..."
+dbstatus=$(php artisan db:show | grep "SQLSTATE\[HY000\] \[2002\]" -c)
+if [[ "$dbstatus" -eq 1 ]]; then
+    echo "Please start the database or check your .env file."
+    exit 1
+fi
+echo "Database: online."
+echo "Done."
+
+
 # doing the base migration
-echo "5/6 running migrations :"
+echo "6/7 running migrations :"
 pam=$({ echo "yes";}|php artisan migrate)
 pamerrorstr="   INFO  Nothing to migrate.  "
 pam=$(echo "$pam" | grep "Nothing")
 if [ "$pam" = "$pamerrorstr" ]; then
-    read -rep "Nothing happened, do you want to run a migrate:fresh ? (y/N) " -i "N" answer
+    read -rep "Nothing happened, do you want to run a migrate:fresh --seed ? (y/N) " -i "N" answer
     if [ "$answer" = "y" ];then
         echo "Running migrate:fresh..."
-        php artisan migrate:fresh
+        php artisan migrate:fresh --seed
         echo "Done."
     else
         echo "Aborting."
         exit 1
     fi
 fi
+
+
 # setting up backpack
 # and
 # creating backpack admin user
 # TODO implement a check if backpack installed succeed or not
-echo "6/6 installing backpack..."
+echo "7/7 installing backpack..."
 { echo "yes"; echo "admin"; echo "admin@email.com"; echo "123";  echo "no" ; echo "no" ; } | php artisan backpack:install > /dev/null
 echo "Done."
+
+
 echo "--INSTALLATION FINISHED--"
 echo "backpack user : admin"
 echo "backpack email : admin@email.com"
