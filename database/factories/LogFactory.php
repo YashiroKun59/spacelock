0d62addf2c6fa5bda30b16bb969e8456886e46dd
matrox471 <matthieuf96@hotmail.com>
2023-01-16 17:55:19 +0100

    added log factory

---
+++ database/factories/LogFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Log>
+ */
+class LogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+        'ip' => fake()->ipv4(),
+        'event_at' => fake()->dateTime(),
+        'event' => fake()->realText(75),
+        ];
+    }
+}
