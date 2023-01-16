ac62191f5c3438a95239159ec5ac7fba44f49c07
matrox471 <matthieuf96@hotmail.com>
2023-01-16 18:27:52 +0100

    bugfix + added environment setup

--- setup.ps1
+++ setup.ps1
@@ -1,18 +1,20 @@
 #!/usr/bin/env pwsh
-# setting up composer
-Write-Output "1/5 Installing composer's addons..."
-composer install >$null 2>&1
-Write-Output "Done."
+Write-Output "1/6 Setting up environment..."
+Remove-Item -Recurse .\vendor\ >$null 2>&1
 # copying the .env.exemple file to .env
-Write-Output "2/5 Copying env file..."
+Write-Output "2/6 Copying env file..."
 Copy-Item .env.example .env
 Write-Output "Done."
+# setting up composer
+Write-Output "3/6 Installing composer's addons..."
+composer install >$null 2>&1
+Write-Output "Done."
 # generating the app key
-Write-Output "3/5 Generating app key"
+Write-Output "4/6 Generating app key"
 php artisan key:generate --ansi >$null 2>&1
 Write-Output "Done."
 # doing the base migration
-Write-Output "4/5 running migrations :"
+Write-Output "5/6 running migrations :"
 $pam=$("yes`r`n"|php artisan migrate)
 $pamerrorstr="   INFO  Nothing to migrate.  "
 $pam=$(Write-Output $pam[1] | findstr "Nothing to migrate")
@@ -32,7 +34,7 @@ if ($pam -eq $pamerrorstr) {
 # and
 # creating backpack admin user
 # TODO implement a check if backpack installed succeed or not
-Write-Output "5/5 installing backpack..."
+Write-Output "6/6 installing backpack..."
 php artisan backpack:install
 Write-Output "Done."
 Write-Output "--INSTALLATION DONE--"
--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 # setting up composer
-echo "1/5 Installing composer's addons..."
-composer install &> /dev/null
+echo "1/6 Setting up the environment..."
+rm ./vendor -r
 echo "Done."
 # copying the .env.exemple file to .env
-echo "2/5 Copying env file..."
+echo "2/6 Copying env file..."
 cp .env.example .env
 echo "Done."
+echo "3/6 Installing composer's addons..."
+composer install &> /dev/null
+echo "Done."
 # generating the app key
-echo "3/5 Generating app key"
+echo "4/6 Generating app key"
 php artisan key:generate --ansi &> /dev/null
 echo "Done."
 # doing the base migration
-echo "4/5 running migrations :"
+echo "5/6 running migrations :"
 pam=$({ echo "yes";}|php artisan migrate)
 pamerrorstr="   INFO  Nothing to migrate.  "
 pam=$(echo "$pam" | grep "Nothing")
@@ -31,7 +34,7 @@ fi
 # and
 # creating backpack admin user
 # TODO implement a check if backpack installed succeed or not
-echo "5/5 installing backpack..."
+echo "6/6 installing backpack..."
 { echo "yes"; echo "admin"; echo "admin@email.com"; echo "123";  echo "no" ; echo "no" ; } | php artisan backpack:install > /dev/null
 echo "Done."
 echo "--INSTALLATION FINISHED--"
