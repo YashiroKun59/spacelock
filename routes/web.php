75c540904657a92f6b3f325deb07a7e4128b99de
Kiwistiti <81768328+Kiwistiti@users.noreply.github.com>
2023-01-18 15:51:20 +0100

    Ajout des controllers payment and Space

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('payements', App\Http\Controllers\PayementController::class);
+Route::resource('spaces', App\Http\Controllers\SpaceController::class);
