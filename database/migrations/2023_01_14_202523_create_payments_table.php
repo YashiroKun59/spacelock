636634d08ffe382cc8c15fd099e2d907c8148053
matrox471 <matthieuf96@hotmail.com>
2023-01-18 20:50:02 +0100

    typo

--- database/migrations/2023_01_14_202523_create_payments_table.php
+++ database/migrations/2023_01_14_202523_create_payments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePayementsTable extends Migration
+class CreatePaymentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreatePayementsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payements');
+        Schema::dropIfExists('payments');
     }
 }
