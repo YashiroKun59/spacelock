2c3971542f42301f329c93bce5c214c0f3cfe072
matrox471 <matthieuf96@hotmail.com>
2023-01-26 00:10:27 +0100

    typo

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,16 +17,19 @@ class UserSeeder extends Seeder
     {
         // demo account
         User::factory()->create([
+            'lastname' => 'administrateur',
             'email' => 'admin@exemple.lan',
             'password' => Hash::make('changeme'),
             'role_id' => 1,
         ]);
         User::factory()->create([
+            'lastname' => 'manager',
             'email' => 'manager@exemple.lan',
             'password' => Hash::make('changeme'),
             'role_id' => 2,
         ]);
         User::factory()->create([
+            'lastname' => 'client',
             'email' => 'customer@exemple.lan',
             'password' => Hash::make('changeme'),
             'role_id' => 3,
