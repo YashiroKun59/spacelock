6604e352460a999482d67145fbdddbd97429ca45
matrox471 <matthieuf96@hotmail.com>
2023-01-25 22:47:33 +0100

    updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property integer $id
- * @property string $uuid
+ * @property integer $role_id
  * @property string $lastname
  * @property string $firstname
  * @property string $address
@@ -31,14 +31,49 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $pm_type
  * @property string $pm_last_four
  * @property string $trial_ends_at
- * @property integer $role_id
  * @property string $created_at
  * @property string $updated_at
+ * @property Rental[] $rentals
+ * @property Site[] $sites
+ * @property Support[] $supports
+ * @property Role $role
  */
 class User extends Model
 {
     /**
      * @var array
      */
-    protected $fillable = ['uuid', 'lastname', 'firstname', 'address', 'zipcode', 'city', 'phone', 'email', 'password', 'reset_password_token', 'reset_password_send_at', 'remember_create_at', 'remember_token', 'signing_count', 'current_singing_at', 'last_signing_at', 'current_signing_ip', 'last_signing_ip', 'comment', 'data_collection', 'enabled', 'stripe_id', 'pm_type', 'pm_last_four', 'trial_ends_at', 'role_id', 'created_at', 'updated_at'];
+    protected $fillable = ['role_id', 'lastname', 'firstname', 'address', 'zipcode', 'city', 'phone', 'email', 'password', 'reset_password_token', 'reset_password_send_at', 'remember_create_at', 'remember_token', 'signing_count', 'current_singing_at', 'last_signing_at', 'current_signing_ip', 'last_signing_ip', 'comment', 'data_collection', 'enabled', 'stripe_id', 'pm_type', 'pm_last_four', 'trial_ends_at', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function rentals()
+    {
+        return $this->hasMany('App\Models\Rental');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sites()
+    {
+        return $this->hasMany('App\Models\Site');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function supports()
+    {
+        return $this->hasMany('App\Models\Support');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
 }
