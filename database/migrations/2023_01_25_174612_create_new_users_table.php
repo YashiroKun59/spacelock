b859aa28a173d93b7dd6a32bc1aba3ace1d0b40d
Augustin Baelde <augustinbaelde@gmail.com>
2023-01-27 21:18:39 +0100

    rajout de champs pour backpack et l'install ok

--- database/migrations/2023_01_25_174612_create_new_users_table.php
+++ database/migrations/2023_01_25_174612_create_new_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('lastname')->nullable();
+            $table->string('name')->nullable();
             $table->string('firstname')->nullable();
             $table->string('address')->nullable();
             $table->string('zipcode')->nullable();
@@ -39,7 +40,7 @@ return new class extends Migration
             $table->string('pm_type')->nullable();
             $table->string('pm_last_four', 4)->nullable();
             $table->timestamp('trial_ends_at')->nullable();
-            $table->foreignID('role_id')->constrained('roles');
+            $table->foreignID('role_id')->default(1)->constrained('roles');
             $table->timestamps();
     });
     }
